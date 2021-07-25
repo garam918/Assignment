2ec9be8f4c287d90e728ecf6cf80d8f0f2ad46b3
garam918 <62510764+garam918@users.noreply.github.com>
2021-07-25 17:11:22 +0900

    Feat : 데이터를 저장할 Activity의 데이터 바인딩 구현

--- app/src/main/java/com/g/autocrypt/MainActivity.kt
+++ app/src/main/java/com/g/autocrypt/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.g.autocrypt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.g.autocrypt.database.CenterEntity
+import com.g.autocrypt.databinding.ActivityMainBinding
+import com.g.autocrypt.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var mainViewModel : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        mainViewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(this.application))
+            .get(MainViewModel::class.java)
+        binding.viewModel = mainViewModel
+
+        mainViewModel.getCenterInfo()
     }
 }
